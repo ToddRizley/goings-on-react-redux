4d8380c988522ce49dc6a43222246e5e96e4778f
ToddRizley <todd.rizley@gmail.com>
2016-09-24 15:06:22 -0400

    get map to render, display markers for each item of itinerary

--- src/containers/EveningContainer.js
+++ src/containers/EveningContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import MapComponent from '../components/MapComponent'
 const Evening = class extends Component {
+
 
 
 
@@ -12,7 +13,12 @@ const Evening = class extends Component {
                 <div>Dinner: {this.props.selectedRestaurant.selectedRestaurant.name} </div>
                 <div>Event: {this.props.selectedEvent.selectedEvent.title} </div>
                 <div>Drinks: {this.props.selectedBar.selectedBar.name} </div>
+                <div><MapComponent barCoords={this.props.selectedBar.selectedBar.location.coordinate}
+                  eventCoords={this.props.selectedEvent.selectedEvent.venue.location}
+                  restCoords={this.props.selectedRestaurant.selectedRestaurant.location.coordinate}/></div>
+
           </div>
+
 
         );
       }
