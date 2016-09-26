48cba90b7298db435040f87ddfa5a054f341d3c5
ToddRizley <todd.rizley@gmail.com>
2016-09-25 21:38:52 -0400

    add material ui to search form, add validation for dates

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import EventListContainer from './containers/EventListContainer.js'
 import SearchForm from './components/SearchForm.js'
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
 
 export default class AppContainer extends Component {
+
 
 
   render() {
     return (
       <div>
-        <center><h1>What Should I Do Tonight?</h1></center>
-        <SearchForm />
-        <EventListContainer />
+          <center><h1>What Should I Do Tonight?</h1></center>
+          <MuiThemeProvider >
+          <SearchForm />
+          </MuiThemeProvider>
+          <br />
+          <br />
+          
+          <EventListContainer />
 
-        {/* <Map/> */}
         </div>
     );
   }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,30 +2,85 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import fetchConcerts from '../actions/fetchConcerts.js'
-
+import DatePicker from 'material-ui/DatePicker'
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 const Form = class extends Component {
+  state = {
+    open: false,
+  };
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
 
   handleFormSubmit(event){
     event.preventDefault()
     var city = document.getElementById("city").value
     var date = document.getElementById("date").value
+
+    if (this.invalidDate(date)) {
+      this.handleOpen()
+    } else {
     this.props.fetchConcerts(city, date)
+    }
   }
 
+  invalidDate(date){
+    var today = new Date();
+    var dateEdit = new Date(date)
+    console.log(dateEdit <= today)
+    if (dateEdit <= today ){
+      return true
+    } else {
+      return false
+    }
+  }
 
   render(){
+    const actions = [
+      <FlatButton
+        label="OK"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
         return (
         <div>
+        <Dialog
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        > Invalid entry
+        </Dialog>
+        <Paper zDepth={2}>
           <center>
             <form onSubmit={this.handleFormSubmit.bind(this)}>
-                <label>City</label>
-                <input type="text" className="entry-input" id="city"/>
-                <label>Date</label>
-                <input type="date" className="entry-input" id="date"/>
-                <button type="submit">Submit</button>
+                <Divider />
+                <TextField
+                    errorText="This field is required"
+                    floatingLabelText="Enter Your City"
+                    id="city"
+                    />
+                <Divider />
+                <DatePicker id="date" errorText="This field is required" hintText="Choose a Date" container="inline" mode="landscape" />
+                <Divider />
+                <RaisedButton label="Submit" type="submit" fullWidth={true} />
+
             </form>
           </center>
+           </Paper>
         </div>
         );
       }
