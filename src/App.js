ba882feb5495c2b7e371dc01fe33b7147eead939
ToddRizley <todd.rizley@gmail.com>
2016-09-28 18:45:08 -0400

    remove Material UI

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import DiningOptionsContainer from './containers/DiningOptionsContainer.js'
 import SearchContainer from './containers/SearchContainer.js'
 import EveningContainer from './containers/EventListContainer.js'
 import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin()
 
 export default class AppContainer extends Component {
 
@@ -20,14 +14,12 @@ export default class AppContainer extends Component {
   render() {
     return (
       <div>
-          <MuiThemeProvider >
+
             <SearchContainer />
             <EventListContainer />
             <DiningOptionsContainer />
             <BarOptionsContainer />
             <EveningContainer />
-          </MuiThemeProvider>
-
         </div>
     );
   }
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import EventListContainer from './EventListContainer.js'
 import SearchForm from '../components/SearchForm.js'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin()
 
 export default class SearchContainer extends Component {
 
@@ -17,9 +11,9 @@ export default class SearchContainer extends Component {
     return (
       <div>
           <center><h1>What Should I Do Tonight?</h1></center>
-          <MuiThemeProvider >
+
           <SearchForm />
-          </MuiThemeProvider>
+
           <br />
           <br />
 
